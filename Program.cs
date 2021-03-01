8d813d9b50b8ffe80b7ce111b54c9c7734161b05
Isaac <isaacgeazy@gmail.com>
2021-03-01 18:44:44 -0300

    Atividade Contagem Regressiva

--- Program.cs
+++ Program.cs
@@ -9,11 +9,14 @@ namespace Contagem_Regressiva
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Preparar...");
             Console.ResetColor();
+            Console.WriteLine();
             Console.ReadKey();
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
             Console.WriteLine("Apontar...");
+            Console.WriteLine();
             Console.ResetColor();
             Console.ReadKey();
+            Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Red;
             Console.Write("FOGO!");
             Console.ResetColor();
