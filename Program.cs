c52c83d70f32c7f36efd40b6f16314abd3ff3543
Isaac <isaacgeazy@gmail.com>
2021-03-01 18:38:20 -0300

    Contagem Regressiva

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Contagem_Regressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Preparar...");
+            Console.ResetColor();
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Apontar...");
+            Console.ResetColor();
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("FOGO!");
+            Console.ResetColor();
+
+    }
+  }
+}
